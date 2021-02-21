539b0a1c1bddde2533ddac9daa64018304861643
misaiahhpro <misaiahh@protonmail.com>
2021-02-21 17:12:40 -0500

    added first writable store

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -50,8 +50,9 @@
 
 <nav>
 	<ul>
-		<li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">home</a></li>
-		<li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">about</a></li>
+		<li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">Login</a></li>
+		<li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">File Upload</a></li>
+    <li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">Service Codes</a></li>
 
 		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
 		     the blog data when we hover over the link or tap it on a touchscreen -->
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,50 +1,25 @@
 <script>
-	import successkid from 'images/successkid.jpg';
+  import successkid from 'images/successkid.jpg';
+  import { files } from '../stores/FileStore';
+
+  let fileArray;
+
+  const unsubscribe = files.subscribe((value) => {
+    fileArray = value;
+  });
 </script>
 
-<style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
-</style>
-
 <svelte:head>
-	<title>Sapper project template</title>
+  <title>Vendor Portal</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<h1>Login</h1>
+<div>{fileArray.length}</div>
 
-<figure>
-	<img alt="Success Kid" src="{successkid}">
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<style>
+  @media (min-width: 480px) {
+    h1 {
+      font-size: 4em;
+    }
+  }
+</style>
